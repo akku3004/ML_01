9a3650a5d37d6859039ad81bdda63365b8a565b4
akku3004 <yaduvanshiaakansha@gmail.com>
2025-05-04 20:49:57 +0530

    Day02

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+student ={1:"Akanksha" ,2:"shubhi" ,3:"kajal"}
+def name(roll):
+    if roll in student:
+        print("name:",student[roll])
+    else:
+        print("Roll number doesn't exist")
+        
