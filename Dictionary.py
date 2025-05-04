759280964bc33a828a7d647a9b1554f1e4e86ab9
akku3004 <yaduvanshiaakansha@gmail.com>
2025-05-04 20:48:45 +0530

    Delete Dictionary.py

--- Dictionary.py
+++
@@ -1,7 +0,0 @@
-student ={1:"Akanksha" ,2:"shubhi" ,3:"kajal"}
-def name(roll):
-    if roll in student:
-        print("name:",student[roll])
-    else:
-        print("Roll number doesn't exist")
-        
