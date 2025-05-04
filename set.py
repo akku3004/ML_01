ca74d49426955924cb0715d6e472450f3bbfb284
akku3004 <yaduvanshiaakansha@gmail.com>
2025-05-04 20:49:10 +0530

    Delete set.py

--- set.py
+++
@@ -1,6 +0,0 @@
-football={"Riya","Aman","Tina"}
-cricket={"Aman","Manshu","Akash"}
-print("Both:" ,football & cricket)
-print("Only one:",football^cricket)
-all_students={"Riya" ,"Aman" ,"Tina" ,"sara" ,"Akash" ,"Manshu"}
-print("None:",all_students-(football|cricket))
