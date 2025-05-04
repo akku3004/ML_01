19da7c30f19f7a61e74405b67add66c71f178392
akku3004 <yaduvanshiaakansha@gmail.com>
2025-05-04 20:49:20 +0530

    Delete tuple.py

--- tuple.py
+++
@@ -1,9 +0,0 @@
-students = [
-   (401,"Mashu",19),
-   (402,"Akash",20),
-   (403,"Aman",19),
-]
-print(" Student Information : ")
-for student in students :
-    roll_no ,name ,age =student
-    print(f"Roll No :{roll_no},Name :{name},Age :{age}")
